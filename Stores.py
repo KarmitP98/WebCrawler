918f51e397511217ca14b8159c5932e641b5be56
Karmit Personal <karmit199@gmail.com>
2020-12-18 10:28:34 -0500

    Minor Updates.

--- Stores.py
+++ Stores.py
@@ -1,24 +1,19 @@
-from bs4 import BeautifulSoup
+import json
+from urllib.request import urlopen
+
 import requests
+from bs4 import BeautifulSoup
 
 if __name__ == '__main__':
-    site = "https://www.amazon.ca/s?k=ssd&ref=nb_sb_noss_1"
+    site = "https://smart-shoppers-2a1ab.firebaseapp.com/MjEotbIqUxReSqfvMjJSY2jirps1/customer/Mxuej6VKhOAbrld9pury"
     # Request to get contents of the Website
     result = requests.get(site).content
     # Create a soup object
     soup = BeautifulSoup(result, "lxml")
 
-    for path_tag in soup.find_all("img"):
-        print(path_tag)
+    page = urlopen(site).read()
+    print(page)
+    data = json.loads(page)
+    print(data)
 
-    # Prints the first occurence of div
-    # print(soup.div)
-    # for div_tag in soup.find_all("div"):
-    #     # if div_tag is not None:
-    #     attrs = div_tag.attrs
-    #     # print(div_tag, "\n", div_tag.attrs, "\n\n")
-    #
-    #     if "class" in attrs:
-    #         print(div_tag["class"])
-    # if div_tag.tag["class"] == "cXedhc":
-    #     print(div_tag)
+    # print(soup)
